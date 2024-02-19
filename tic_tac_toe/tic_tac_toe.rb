87f0e80cd48a682933f33049e14fa111d1992341
tonycharles377 <tonycharles377@gmail.com>
2024-02-19 20:11:28 +0300

    Helper methods

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -23,6 +23,38 @@ class TicTacToe
     puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
   
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(token, index)
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    if @board[index] == nil
+      false
+    else
+      true
+    end
+  end
+
+  def valid_move?(index)
+    if !position_taken?(index) && index.include?(0, 8)
+      true
+    else
+      false
+    end
+  end
+
+  def turn_count
+    @board.compact.length
+  end
+
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
+
 end
 
 game = TicTacToe.new
