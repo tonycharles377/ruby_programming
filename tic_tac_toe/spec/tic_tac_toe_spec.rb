94df7c69214724530a977ef1a6cfe17e31fdf6f8
tonycharles377 <tonycharles377@gmail.com>
2024-05-08 00:06:43 +0300

    Add test for a win

--- tic_tac_toe/spec/tic_tac_toe_spec.rb
+++ tic_tac_toe/spec/tic_tac_toe_spec.rb
@@ -9,4 +9,11 @@ describe TicTacToe do
             expect { game.play }.to output(/It's a draw!/).to_stdout
         end
     end
+
+    it"Outputs Congratulations player X/o won!" do
+        moves = ['1','2','3','4','5','6','7']
+        allow_any_instance_of(Kernel).to receive(:gets).and_return(*moves)
+        game = TicTacToe.new
+        expect {game.play}.to output(/Congratulations X won!/ || /Congratulations O won!/).to_stdout
+    end
 end
