074888b540061abf168e50ffe638a6b1c3945146
tonycharles377 <tonycharles377@gmail.com>
2024-05-08 18:28:30 +0300

    Write a test for case - Handle a right_shift greater than 26

--- caesar_cipher/spec/caesar_cipher_spec.rb
+++ caesar_cipher/spec/caesar_cipher_spec.rb
@@ -16,4 +16,8 @@ RSpec.describe "caesar_cipher" do
     it "Handles a negative right_shift value" do
         expect(caesar_cipher("It is what it is!!", -20)).to eql("Oz oy cngz oz oy!!")
     end
+
+    it "handles a right_shift greater than 26" do
+        expect(caesar_cipher("Hello", 30)).to eq("Lipps")
+    end
 end
