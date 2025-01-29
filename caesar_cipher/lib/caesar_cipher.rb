25f7458964e7871dc2c99729d5c99ca758873f44
tonycharles377 <tonycharles377@gmail.com>
2025-01-29 18:06:47 +0300

    Adding rubocop

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -1,19 +1,21 @@
-# Implement a caesar cipher that takes in a string and the shift factor 
+# frozen_string_literal: true
+
+# Implement a caesar cipher that takes in a string and the shift factor
 # and then outputs the modified string
 
 def caesar_cipher(string, right_shift)
-  encoded = ""
+  encoded = ''
 
   string.each_char do |char|
     if char.match?(/[a-zA-Z]/)
-      ascii_offset = char.match(/[a-z]/)? 97 : 65
+      ascii_offset = char.match(/[a-z]/) ? 97 : 65
       encoded << (ascii_offset + (char.ord - ascii_offset + right_shift) % 26).chr
     else
       encoded << char
     end
   end
   puts encoded
-  return encoded
+  encoded
 end
 
-caesar_cipher("What a string!", 5)
+caesar_cipher('What a string!', 5)
